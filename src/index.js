b9f0779ca465f06486fb6f3c95d8208e7e52b029
Margaret Huang <margaret@goodrx.com>
2021-08-23 22:12:46 -0700

    Add basic todo list

--- src/index.js
+++ src/index.js
@@ -3,31 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Todo(props) {
+  //Checkbox is checked?
+  //Save text list
+
+  /*
+  {data = [{task,done},...]}
+  {
+    'task': 'blah',
+    'done': bool
+  }
+  */
   return (
     <div>
-      <input type="checkbox"></input>
-      <input type="text"></input>
+      <input checked={props.done} type="checkbox"></input>
+      <input type="text" value={props.task}></input>
+      <input type="text" value="BLAH"></input>
     </div>
   );
 }
   
   class Board extends React.Component {
-
-    handleClick(i) {
-      }
-
-    create_moretodo(){
-      return (
-          <div className="todo">
-            <Todo />
-          </div>
-      );
+    constructor(props) {
+      super(props);
+      // This binding is necessary to make `this` work in the callback
+      this.createTODOitems = this.createTODOitems.bind(this);
+      this.state = {
+        isClicked: false,
+        tasks: [{task: "HLELO", done: true}]
+      };
     }
-  
+
+    createTODOitems(){
+      console.log("IVE BEENN CLICKED");
+      this.setState({
+        isClicked: true,
+      });
+      // best practice for updating state?
+      this.state.tasks.push({task: null, done: false});
+    }
+
     render() {
+      const todoItems = this.state.tasks.map((task) =>
+        <Todo task={task.task} done={task.done}/>
+      );
+      console.log(this.state);
+
       return (
         <div>
-          <button onclick="create_moretodo()">ADD TODO</button>
+          <button onClick={this.createTODOitems}>ADD TODO</button>
+          {todoItems}
         </div>
       );
     }
